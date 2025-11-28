edc0f28a2ca1415f25489c5e4fe00f254832bf22
aayusht200 <aayush.t200@gmail.com>
2025-11-28 13:31:19 +0530

    fibonacci using both methods

--- Recursion/index.js
+++ Recursion/index.js
@@ -54,7 +54,47 @@ function flatArr(arr) {
     }
 }
 
-function revStr(arr) {}
+function revStr(str) {
+    if (str.length == 0) return '';
+    return str.slice(-1) + revStr(str.slice(0, -1));
+}
+function fibSeries(num) {
+    if (num == 0) return [];
+    if (num == 1) return [0];
+    let temp = [0, 1];
+    for (let i = 2; i < num; i++) {
+        temp.push(temp[i - 1] + temp[i - 2]);
+    }
+    return temp;
+}
+function recurFibSeries(num) {
+    if (num == 0) return [];
+    if (num == 1) return [0];
+    if (num == 2) return [0, 1];
+    const prev = recurFibSeries(num - 1);
+    const next = prev[prev.length - 1] + prev[prev.length - 2];
+    return [...prev, next];
+}
+
+function recursiveFib(num) {
+    if (num == 0) return 0;
+    if (num == 1) return 1;
+    return recursiveFib(num - 1) + recursiveFib(num - 2);
+}
+
+function ittrativeFib(num) {
+    if (num == 0) return 0;
+    if (num == 1) return 1;
+    let prev = 0;
+    let curr = 1;
+    let next;
+    for (let i = 2; i <= num; i++) {
+        next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    return next;
+}
 
 let arr = [1, 2, 3, 4, 5, 6];
 let sumarr = [1, 2, 3, 4, 5, 6];
@@ -67,3 +107,8 @@ console.log(`Sum of ${sumarr} :  ${sumArr(sumarr)}`);
 console.log(`Max number from [7, 71, 2, 3, 24, 85, 6]:${maxNumArr([7, 71, 2, 3, 24, 85, 6])}`);
 console.log(`Number of even elements [7, 71, 2, 3, 24, 85, 6, 9, 10]:${countEven([7, 71, 2, 3, 24, 85, 6, 9, 10])}`);
 console.log(`Flattned Arr[1, [2, [3, 4], 5], 6]:${flatArr([1, [2, [3, 4], 5], 6])}`);
+console.log(`Str Rev hello :${revStr('hello')}`);
+console.log(`Fibonacci seq of 5 using itrative method: ${fibSeries(5)}`);
+console.log(`Fibonacci seq of 5 using recursive method: ${recurFibSeries(5)}`);
+console.log(`Fibonacci number 5 using recursive method: ${recursiveFib(5)}`);
+console.log(`Fibonacci number 5 using itrative method: ${ittrativeFib(5)}`);
