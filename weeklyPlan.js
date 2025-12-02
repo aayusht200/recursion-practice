668807cbcb28d06c61d27c755f3aca6c9c53350c
aayusht200 <aayush.t200@gmail.com>
2025-12-02 11:15:37 +0530

    day 3

--- weeklyPlan.js
+++ weeklyPlan.js
@@ -13,7 +13,7 @@ function factorial(num) {
 console.log(factorial(4));
 
 function revStr(str) {
-    if (str.length === 1) return str;
+    if (str.length == 1) return str;
     return str.slice(-1) + revStr(str.slice(0, -1));
 }
 console.log(revStr('Hello'));
@@ -26,26 +26,57 @@ console.log(revStr('Hello'));
 // - Flatten a nested array (simple version)
 
 function sumNum(arr) {
-    if (arr.length === 1) return arr[0];
-    return arr[arr.length - 1] + sumNum(arr.slice(0, arr.length - 1));
+    if (arr.length === 0) return 0;
+    return arr[0] + sumNum(arr.slice(1));
 }
 
 function countValue(arr, value) {
     let count = 0;
     if (arr.length === 0) return 0;
-    if (arr[arr.length - 1] === value) {
-        count = 1 + countValue(arr.slice(0, arr.length - 1), value);
-    } else count = countValue(arr.slice(0, arr.length - 1), value);
-    return count;
+    if (arr[0] === value) count++;
+    return count + countValue(arr.slice(1), value);
 }
-console.log(sumNum([1, 4, 5, 7, 9]));
-console.log(countValue([1, 3, 3, 5], 3));
 
 function flattenArr(arr) {
-    if (arr.length === 0) return [];
-    if (typeof arr[0] === 'number') {
-        return [arr[0], ...flattenArr(arr.slice(1))];
-    } else return [...flattenArr(arr[0]), ...flattenArr(arr.slice(1))];
+    if (arr.length == 0) return [];
+    if (typeof arr[0] === 'number') return [arr[0], ...flattenArr(arr.slice(1))];
+    return [...flattenArr(arr[0]), ...flattenArr(arr.slice(1))];
 }
 
+console.log(sumNum([1, 4, 5, 7, 9]));
+console.log(countValue([1, 3, 3, 3, 5], 3));
 console.log(flattenArr([1, [2, [3, 4], 5], 6]));
+
+// ## Day 3 — Objects
+// Goal: Handle nested objects.
+// Tasks:
+// - Get total number of keys in a nested object
+// - Find a key inside a deeply nested object
+
+function countKeys(data) {
+    let count = 0;
+    if (Object.keys(data).length === 0) return 0;
+    let [firstKey, firstData] = Object.entries(data)[0];
+    console.log(typeof firstData);
+
+    // if (typeof firstData === 'object') console.log(firstData);
+}
+
+let data = {
+    id: 101,
+    profile: {
+        personal: {
+            first: 'Aayush',
+            last: 'T',
+        },
+        education: {
+            degree: 'BCA',
+            year: 2024,
+        },
+    },
+    status: 'active',
+};
+countKeys(data);
+// Object.entries(data).forEach((ele) => {
+//     console.log(typeof ele);
+// });
